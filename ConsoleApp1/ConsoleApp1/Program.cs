c102c2e8e7ce949212ee326e2173989e9575a0b4
Egor <arnoldtagirov1@gmail.com>
2025-11-07 16:37:39 +0100

    -m

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,14 +1,23 @@
 ﻿
+using System;
+Action ShowTimeAction = () => Console.WriteLine("Time:" + DateTime.Now.ToString("HH:mm:ss"));
+Action ShowDayOfTheWeekAction = () => Console.WriteLine("Day: " + DateTime.Now.DayOfWeek);
+Action ShowDataAction = () => Console.WriteLine("Data: " + DateOnly.FromDateTime(DateTime.Now).ToString());
+
+Func<double, double, double> S_Triangle = (a, b) => 0.5 * a * b;
+Func<double,double,double>  S_Rectangle= (a,b) => a*b;
+//double res = S_Triangle(1,2);
+#region 1
+
 SearchDelegateType X = GetEven;
 SearchDelegateType Y = GetOdd;
 DelegateISType Z = IsPrime;
-
 int[] GetEven(int[] arr)
 {
-    List<int> res= new List<int>();
-    foreach(int i in arr)
+    List<int> res = new List<int>();
+    foreach (int i in arr)
     {
-        if (i % 2 ==  0)
+        if (i % 2 == 0)
         {
             res.Add(i);
         }
@@ -29,6 +38,28 @@ int[] GetOdd(int[] arr)
     return res.ToArray();
 }
 
+bool IsPerfectSquare(long x)
+{
+    long s = (long)Math.Sqrt(x);
+    return s * s == x;
+}
+
+int[] GetFibonacci(int[] arr)
+{
+    List<int> res = new List<int>();
+    foreach (int n in arr)
+    {
+        if (n < 0) break;
+        long x1 = 5L * n * n + 4;
+        long x2 = 5L * n * n - 4;
+        if (IsPerfectSquare(x1) || IsPerfectSquare(x2))
+        {
+            res.Add(n);
+        }
+    }
+    return arr.ToArray();
+}
+
 bool IsPrime(int n)
 {
     if (n < 2) return false;
@@ -36,7 +67,10 @@ bool IsPrime(int n)
         if (n % i == 0) return false;
     return true;
 }
-
+Func<int[], int[]> fibonacci = GetFibonacci;
+Predicate<int> isPrimePredicat = IsPrime;
 
 public delegate bool DelegateISType(int n);
-public delegate  int[] SearchDelegateType(int[] arr);
+public delegate int[] SearchDelegateType(int[] arr);
+#endregion
+
