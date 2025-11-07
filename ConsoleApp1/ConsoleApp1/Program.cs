da7409c6926a3a36e5b8231dfad7888c52d0da9e
Egor <arnoldtagirov1@gmail.com>
2025-11-07 15:30:48 +0100

    -m

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+SearchDelegateType X = GetEven;
+SearchDelegateType Y = GetOdd;
+DelegateISType Z = IsPrime;
+
+int[] GetEven(int[] arr)
+{
+    List<int> res= new List<int>();
+    foreach(int i in arr)
+    {
+        if (i % 2 ==  0)
+        {
+            res.Add(i);
+        }
+    }
+    return res.ToArray();
+}
+
+int[] GetOdd(int[] arr)
+{
+    List<int> res = new List<int>();
+    foreach (int i in arr)
+    {
+        if (i % 2 != 0)
+        {
+            res.Add(i);
+        }
+    }
+    return res.ToArray();
+}
+
+bool IsPrime(int n)
+{
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+
+
+public delegate bool DelegateISType(int n);
+public delegate  int[] SearchDelegateType(int[] arr);
